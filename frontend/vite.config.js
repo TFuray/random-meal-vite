87eb86417c4e944ec76c79d1892b2c7d3001c852
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-03-01 02:47:51 -0800

    upload framework

---
+++ frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3456'
+    },
+
+    // to serve on the 3000 port
+    host: '0.0.0.0',
+    port: 3000
+  },
+  plugins: [react()]
+})
+
