069906a39e35be442c5c1c3cd72ceb4ed570b2c4
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-03-12 10:47:08 -0700

    included posting meal

--- frontend/src/pages/ShareMeal.jsx
+++ frontend/src/pages/ShareMeal.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
-import { useSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom"
-import { toast } from 'react-toastify'
 import { newMeal } from "../features/meal/mealSlice"
 
 
