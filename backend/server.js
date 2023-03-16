fb7cdb8e0ab637bd63a43b768d9193caf8b593bd
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-03-16 13:41:54 -0700

    cyclic ready

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ app.use('/api/meal', require('./routes/mealDbRoutes'))
 
 // serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')))
 }
 
 app.use(errorHandler)
