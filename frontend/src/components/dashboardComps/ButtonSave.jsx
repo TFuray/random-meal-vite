e31aea11ff76e26c5929f19ef2eb53ef2efc90d5
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-04-16 20:00:05 -0700

    type

--- frontend/src/components/dashboardComps/ButtonSave.jsx
+++ frontend/src/components/dashboardComps/ButtonSave.jsx
@@ -1,17 +1,17 @@
 import { useDispatch, useSelector } from "react-redux"
-import { saveMeal } from '../../features/meal/mealSlice'
-import { toast } from 'react-toastify'
+import { saveMeal } from "../../features/meal/mealSlice"
+import { toast } from "react-toastify"
 
 const ButtonSave = ({ randomMeal, clicked, setClicked }) => {
   const dispatch = useDispatch()
   const meal = JSON.stringify(randomMeal)
-  const { user } = useSelector(state => state.auth)
+  const { user } = useSelector((state) => state.auth)
 
   const onSubmit = (e) => {
     e.preventDefault()
     if (!user) {
       toast.warn("Login To Do That", {
-        position: toast.POSITION.TOP_CENTER
+        position: toast.POSITION.TOP_CENTER,
       })
     }
     dispatch(saveMeal({ meal }))
@@ -31,16 +31,24 @@ const ButtonSave = ({ randomMeal, clicked, setClicked }) => {
           value={meal}
           readOnly
         />
-        {!clicked ? <button type="submit" onClick={() => handleClick()} className="btn btn-secondary flex justify-center space-x-2 ml-5">
-          Save this meal
-        </button> :
-
-          <button type="submit" onClick={() => handleClick()} className="btn btn-secondary btn-disabled flex justify-center space-x-2 ml-5">
+        {!clicked ? (
+          <button
+            type="submit"
+            onClick={() => handleClick()}
+            className="btn btn-secondary flex justify-center space-x-2 ml-5"
+          >
+            Save this meal
+          </button>
+        ) : (
+          <button
+            type="submit"
+            onClick={() => handleClick()}
+            className="btn btn-secondary btn-disabled flex justify-center space-x-2 ml-5"
+          >
             Meal already saved
-          </button>}
+          </button>
+        )}
       </form>
-
-
     </section>
   )
 }
