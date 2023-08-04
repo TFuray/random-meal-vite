40a717cf0f9a670f27d51c20acebb4477e6d3bad
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-08-04 12:37:54 -0700

    changed random meal layout and display meal

--- frontend/src/components/dashboardComps/DisplayMeal.jsx
+++ frontend/src/components/dashboardComps/DisplayMeal.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs"
+import "react-tabs/style/react-tabs.css"
+
 const DisplayMeal = ({ randomMeal }) => {
+  const [showMore, setShowMore] = useState(false)
   return (
     <>
       {randomMeal.map(
@@ -56,53 +61,72 @@ const DisplayMeal = ({ randomMeal }) => {
             .filter(({ ingredient }) => ingredient)
             .slice(0, 20)
           return (
-            <div
-              className="hero card shadow-2xl glass rounded-3xl"
-              key={idMeal}
-            >
-              <div className="hero-content flex-col lg:flex-row">
-                <img
-                  src={strMealThumb}
-                  className="max-w-sm rounded-lg mr-2 shadow-2xl"
-                  alt={`Image of ${strMeal}`}
-                />
-                <div>
-                  <h1 className="text-5xl font-bold">
-                    {strMeal}
-                    <span className="inline-block ml-3 ">
-                      <a
-                        href={strSource}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <svg
-                          className="fill-current hover:fill-primary-content"
-                          width="24"
-                          height="24"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                        >
-                          <path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
-                        </svg>
-                      </a>
-                    </span>
-                  </h1>
-                  <ul className="grid grid-cols-3 gap-2 place-items-start pt-6">
-                    {ingredients.map(({ ingredient, measure }, i) => (
-                      <li key={i}>
-                        <span className="font-bold grid">{ingredient}</span>{" "}
-                        {measure}
-                      </li>
-                    ))}
-                  </ul>
-                  <h2 className="flex pt-6 w-4/5 text-center text-2xl font-bold">
-                    Instructions:
-                  </h2>
-                  <p className="flex pb-6 w-4/5 text-start line-clamp-4">
-                    {strInstructions}
-                  </p>
-                  <button className="text-info hover:text-blue-500">Show More</button>
+            <div className="mx-auto px-4">
+              <div className=" flex ">
+                <div className="grid flex-shrink-0 place-items-center my-20 lg:pr-6">
+                  <div className="carousel max-w-[400px] rounded-xl">
+                    <div
+                      id="slide1"
+                      className="carousel-item relative"
+                    >
+                      <img
+                        src={strMealThumb}
+                        className="w-[400px] h-[500px] object-cover"
+                        alt={`Image of ${strMeal}`}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="divider lg:divider-horizontal" />
+                <div className="flex w-[500px] flex-col justify-start mt-14">
+                  <div className="">
+                    <h1 className="text-3xl mb-10 font-bold">{strMeal}</h1>
+                  </div>
+                  <Tabs>
+                    <TabList>
+                      <Tab><h2 className="text-lg">Ingredients</h2></Tab>
+                      <Tab><h2 className="text-lg">Instructions</h2></Tab>
+                    </TabList>
+
+                    <TabPanel>
+                      <ul className="grid grid-cols-3 gap-2 place-items-start pt-6">
+                        {ingredients.map(({ ingredient, measure }, i) => (
+                          <li key={i}>
+                            <span className="font-bold grid">{ingredient}</span>{" "}
+                            {measure}
+                          </li>
+                        ))}
+                      </ul>
+                    </TabPanel>
+                    <TabPanel>
+                      {!showMore ? (
+                        <>
+                          <ul className="">
+                            <li className="line-clamp-4 leading-relaxed text-lg">{strInstructions}</li>
+                          </ul>
+                          <button
+                            onClick={() => setShowMore(!showMore)}
+                            className="text-info hover:text-blue-500"
+                          >
+                            Show More
+                          </button>
+                        </>
+                      ) : (
+                        <>
+                          <p className="leading-relaxed text-lg">
+                            {strInstructions}
+                          </p>
+                          <button
+                            onClick={() => setShowMore(!showMore)}
+                            className="text-info hover:text-blue-500"
+                          >
+                            Show Less
+                          </button>
+                        </>
+                      )}
+                    </TabPanel>
+                  </Tabs>
                 </div>
               </div>
             </div>
--- frontend/src/components/savedMealComps/MealCard.jsx
+++ frontend/src/components/savedMealComps/MealCard.jsx
@@ -16,13 +16,13 @@ const MealCard = ({ meal, savedMeal }) => {
   const openCommentModal = () => setIsCommentOpen(true)
   const closeCommentModal = () => setIsCommentOpen(false)
 
+
   return (
     <>
       {savedMeal ? (
-        <div className="card w-auto bg-primary bg-opacity-50 shadow-xl">
-          <div className="static mt-2 mr-8 ">
-            <div className="static">
-              <figure className="pl-10 pt-10 hover:cursor-pointer">
+        <div className="card bg-base-200 shadow-xl">
+          <div >
+              <figure className="px-10 pt-10 hover:cursor-pointer">
                 <img
                   onClick={openModal}
                   src={meal.strMealThumb}
@@ -30,19 +30,18 @@ const MealCard = ({ meal, savedMeal }) => {
                   className="rounded-xl"
                 />
               </figure>
-            </div>
             <div className="absolute top-0 right-0 pt-10 pr-5">
               <button
                 onClick={() => dispatch(deleteMeal(savedMeal._id))}
-                className="btn btn-circle hover:bg-red-600 "
+                className="btn btn-circle btn-sm hover:bg-red-600 "
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  strokeWidth=""
+                  strokeWidth="2"
                   stroke=""
-                  className="w-8 h-8 stroke-white"
+                  className="w-3 h-3 stroke-white"
                 >
                   <path
                     strokeLinecap="round"
@@ -55,7 +54,16 @@ const MealCard = ({ meal, savedMeal }) => {
           </div>
 
           <div className="card-body items-center text-center">
-            <h2 className="card-title">{meal.strMeal}</h2>
+            <h2 className="card-title">{meal.strMeal}
+            </h2>
+            <div className="flex gap-2">
+              <div className="badge badge-info text-xs">
+                {meal.strArea}
+              </div>
+              <div className="badge badge-accent text-xs">
+                {meal.strCategory}
+              </div>
+            </div>
             <div>
               <StarRating
                 meal={savedMeal}
@@ -96,37 +104,10 @@ const MealCard = ({ meal, savedMeal }) => {
                 />
               </figure>
             </div>
-            {/* <div className="absolute top-0 right-0 pt-10 pr-5">
-              <button
-                onClick={() => dispatch(deleteMeal(savedMeal._id))}
-                className="btn btn-circle hover:bg-red-600 "
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth=""
-                  stroke=""
-                  className="w-8 h-8 stroke-white"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div> */}
           </div>
 
           <div className="card-body items-center text-center">
             <h2 className="card-title">{meal.strMeal}</h2>
-            {/* <div>
-              <StarRating
-                meal={savedMeal}
-                stars={savedMeal.rating}
-              />
-            </div> */}
             <div className="card-actions">
               <button
                 onClick={openModal}
